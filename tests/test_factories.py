34ba3a0c6c7b140a30fb0fa9efd2dee601ed9095
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-02-24 20:44:54 -0500

    Add test for factory

---
+++ tests/test_factories.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from tests.factories import RecommendationFactory
+from service.models import Recommendation, RecommendationType
+
+
+# pylint: disable=too-many-public-methods
+class TestFactories(TestCase):
+    """Test Cases for Factories"""
+
+    def test_recommendation_factory_creation(self):
+        """It should return recommendation with random data"""
+        recommendation = RecommendationFactory()
+        self.assertTrue(recommendation is not None)
+        self.assertEqual(recommendation.id, None)
+        self.assertTrue(recommendation.product_a_sku is not None)
+        self.assertTrue(recommendation.product_b_sku is not None)
+        self.assertTrue(recommendation.type in RecommendationType)
