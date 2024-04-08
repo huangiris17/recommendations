2aeb73556862461dab92cdf779ac8a6a28cfe084
Ruochen Miao <boomber_man_@outlook.com>
2024-04-08 18:30:37 +0000

    attempts to fix port issue

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,22 @@
 """
 Environment for Behave Testing
 """
+
 from os import getenv
 from selenium import webdriver
 
-WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
-DRIVER = getenv('DRIVER', 'chrome').lower()
+WAIT_SECONDS = int(getenv("WAIT_SECONDS", "60"))
+PORT = getenv("PORT", "8000")
+BASE_URL = getenv("BASE_URL", f"http://localhost:{PORT}")
+DRIVER = getenv("DRIVER", "chrome").lower()
 
 
 def before_all(context):
-    """ Executed once before all tests """
+    """Executed once before all tests"""
     context.base_url = BASE_URL
     context.wait_seconds = WAIT_SECONDS
     # Select either Chrome or Firefox
-    if 'firefox' in DRIVER:
+    if "firefox" in DRIVER:
         context.driver = get_firefox()
     else:
         context.driver = get_chrome()
@@ -23,12 +25,14 @@ def before_all(context):
 
 
 def after_all(context):
-    """ Executed after all tests """
+    """Executed after all tests"""
     context.driver.quit()
+
 
 ######################################################################
 # Utility functions to create web drivers
 ######################################################################
+
 
 def get_chrome():
     """Creates a headless Chrome driver"""
